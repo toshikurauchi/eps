0bb7fa8ce733eaeedb8117dd0d34684007b9c991
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2010-11-25 17:11:23 -0200

    Chamando value iteration e imprimindo a politica escolhida; excluindo doctests.py; lendo probabilidades dos movimentos

--- src/main.py
+++ src/main.py
@@ -4,49 +4,70 @@ Created on Nov 23, 2010
 @author: Andrew Kurauchi
 '''
 import sys
+import mdp
 
 def main():
-    inputFileName = sys.argv[1]
-    inputFile = open(inputFileName, 'r')
+    input_file_name = sys.argv[1]
+    input_file = open(input_file_name, 'r')
     "Read targets coordinates"
-    numberOfTargets = int(inputFile.readline())
+    number_of_targets = int(input_file.readline())
     targets = []
-    for i in range(numberOfTargets):
-        targets.append((int(inputFile.readline()),int(inputFile.readline())))
-    numberOfForbiddens = int(inputFile.readline())
+    for i in range(number_of_targets):
+        targets.append((int(input_file.readline()),int(input_file.readline())))
+    "Read forbidden states coordinates"
+    number_of_forbiddens = int(input_file.readline())
     forbidden = []
-    for i in range(numberOfForbiddens):
-        forbidden.append((int(inputFile.readline()),int(inputFile.readline())))
-    start = (int(inputFile.readline()),int(inputFile.readline()))
-    numberOfObstacles = int(inputFile.readline())
+    for i in range(number_of_forbiddens):
+        forbidden.append((int(input_file.readline()),int(input_file.readline())))
+    terminals = list(targets)
+    terminals += forbidden
+    "Read initial position"
+    start = (int(input_file.readline()),int(input_file.readline()))
+    "Read obstacles coordinates"
+    number_of_obstacles = int(input_file.readline())
     obstacles = []
-    for i in range(numberOfObstacles):
-        obstacles.append((int(inputFile.readline()),int(inputFile.readline())))
-    targetReward = float(inputFile.readline())
-    forbiddenReward = float(inputFile.readline())
-    movementReward = float(inputFile.readline())
-    grid = generateGrid(targets, forbidden, obstacles, targetReward, forbiddenReward, movementReward)
-    print 'Targets: ', numberOfTargets
+    for i in range(number_of_obstacles):
+        obstacles.append((int(input_file.readline()),int(input_file.readline())))
+    "Read reward value for getting to a target"
+    target_reward = float(input_file.readline())
+    "Read reward value for getting to a forbidden state"
+    forbidden_reward = float(input_file.readline())
+    "Read movement cost"
+    movement_reward = float(input_file.readline())
+    "Read probabilities"
+    probabilities = []
+    for i in range(4):
+        probabilities.append(float(input_file.readline()))
+    input_file.close()
+    grid = generate_grid(targets, forbidden, obstacles, target_reward, forbidden_reward, movement_reward)
+    print 'Targets: ', number_of_targets
     print 'Target array: ', targets
-    print 'Forbidden points: ', numberOfForbiddens
+    print 'Forbidden states: ', number_of_forbiddens
     print 'Forbidden: ', forbidden
+    print 'Terminals: ', terminals
     print 'Start: ', start
-    print 'Obstacles: ', numberOfObstacles
+    print 'Obstacles: ', number_of_obstacles
     print 'Obstacle array: ', obstacles
-    print 'Target reward: ', targetReward
-    print 'Forbidden reward: ', forbiddenReward
-    print 'Movement reward: ', movementReward
+    print 'Target reward: ', target_reward
+    print 'Forbidden reward: ', forbidden_reward
+    print 'Movement reward: ', movement_reward
+    print 'Probabilities: ', probabilities
     print 'Grid: '
     for line in grid:
         print line
+    grid_mdp = mdp.GridMDP(grid, terminals, start)
+    U = mdp.value_iteration(grid_mdp, 0.1)
+    pi = mdp.best_policy(grid_mdp, U)
+    for line in grid_mdp.to_arrows(pi):
+        print line
     
-def generateGrid(targets, forbiddenPoints, obstacles, targetReward, forbiddenReward, movementReward):
+def generate_grid(targets, forbidden_states, obstacles, target_reward, forbidden_reward, movement_reward):
     "Grid's rows = y, cols = x and the grid is stored reversed"
-    grid = [[movementReward]*5, [movementReward]*5, [movementReward]*5, [movementReward]*5, [movementReward]*5]
+    grid = [[movement_reward]*5, [movement_reward]*5, [movement_reward]*5, [movement_reward]*5, [movement_reward]*5]
     for target in targets:
-        grid[target[1]][target[0]] = targetReward
-    for forbidden in forbiddenPoints:
-        grid[forbidden[1]][forbidden[0]] = forbiddenReward
+        grid[target[1]][target[0]] = target_reward
+    for forbidden in forbidden_states:
+        grid[forbidden[1]][forbidden[0]] = forbidden_reward
     for obstacle in obstacles:
         grid[obstacle[1]][obstacle[0]] = None
     grid.reverse()
--- src/mdp.py
+++ src/mdp.py
@@ -44,8 +44,9 @@ class GridMDP(MDP):
     specify the grid as a list of lists of rewards; use None for an obstacle
     (unreachable state).  Also, you should specify the terminal states.
     An action is an (x, y) unit vector; e.g. (1, 0) means move east."""
-    def __init__(self, grid, terminals, init=(0, 0), gamma=.9):
+    def __init__(self, grid, terminals, init=(0, 0), probabilities=[0.8, 0.1, 0.1, 0.0], gamma=.9):
         grid.reverse() ## because we want row 0 on bottom, not on top
+        self.probabilities = probabilities ## [up, right, left, down]
         MDP.__init__(self, init, actlist=orientations, 
                      terminals=terminals, gamma=gamma)
         update(self, grid=grid, rows=len(grid), cols=len(grid[0]))
@@ -57,11 +58,11 @@ class GridMDP(MDP):
 
     def T(self, state, action):
         if action == None:
-            return [(0.0, state)]
+            return [(self.probabilities[3], state)]
         else:
-            return [(0.8, self.go(state, action)),
-                    (0.1, self.go(state, turn_right(action))),
-                    (0.1, self.go(state, turn_left(action)))]
+            return [(self.probabilities[0], self.go(state, action)),
+                    (self.probabilities[1], self.go(state, turn_right(action))),
+                    (self.probabilities[2], self.go(state, turn_left(action)))]
 
     def go(self, state, direction):
         "Return the state that results from going in this direction."
@@ -138,34 +139,3 @@ def policy_evaluation(pi, U, mdp, k=20):
         for s in mdp.states:
             U[s] = R(s) + gamma * sum([p * U[s1] for (p, s1) in T(s, pi[s])])
     return U
-
-__doc__ += """
->>> pi = best_policy(Fig[17,1], value_iteration(Fig[17,1], .01))
-
->>> Fig[17,1].to_arrows(pi)
-[['>', '>', '>', '.'], ['^', None, '^', '.'], ['^', '>', '^', '<']]
-
->>> print_table(Fig[17,1].to_arrows(pi))
->   >      >   .  
-^   None   ^   .  
-^   >      ^   <  
-
->>> print_table(Fig[17,1].to_arrows(policy_iteration(Fig[17,1])))
->   >      >   .  
->   None   <   .  
->   >      >   v  
-"""
-
-__doc__ += random_tests("""
->>> pi
-{(3, 2): None, (3, 1): None, (3, 0): (-1, 0), (2, 1): (0, 1), (0, 2): (1, 0), (1, 0): (1, 0), (0, 0): (0, 1), (1, 2): (1, 0), (2, 0): (0, 1), (0, 1): (0, 1), (2, 2): (1, 0)}
-
->>> value_iteration(Fig[17,1], .01)
-{(3, 2): 1.0, (3, 1): -1.0, (3, 0): 0.12958868267972745, (0, 1): 0.39810203830605462, (0, 2): 0.50928545646220924, (1, 0): 0.25348746162470537, (0, 0): 0.29543540628363629, (1, 2): 0.64958064617168676, (2, 0): 0.34461306281476806, (2, 1): 0.48643676237737926, (2, 2): 0.79536093684710951}
-
->>> policy_iteration(Fig[17,1])
-{(3, 2): None, (3, 1): None, (3, 0): (0, -1), (2, 1): (-1, 0), (0, 2): (1, 0), (1, 0): (1, 0), (0, 0): (1, 0), (1, 2): (1, 0), (2, 0): (1, 0), (0, 1): (1, 0), (2, 2): (1, 0)}
- 
-""")
-
-
