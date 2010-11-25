196137fd29cab05f5fa70ab25971147effe6087c
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2010-11-25 11:29:25 -0200

    Gerando grid

--- src/main.py
+++ src/main.py
@@ -12,21 +12,35 @@ def main():
     target = (int(inputFile.readline()),int(inputFile.readline()))
     forbidden = (int(inputFile.readline()),int(inputFile.readline()))
     start = (int(inputFile.readline()),int(inputFile.readline()))
-    obstacles = int(inputFile.readline())
-    obstacle = []
-    for i in range(obstacles):
-        obstacle.append((int(inputFile.readline()),int(inputFile.readline())))
+    numberOfObstacles = int(inputFile.readline())
+    obstacles = []
+    for i in range(numberOfObstacles):
+        obstacles.append((int(inputFile.readline()),int(inputFile.readline())))
     targetReward = float(inputFile.readline())
     forbiddenReward = float(inputFile.readline())
     movementReward = float(inputFile.readline())
+    grid = generateGrid(target, forbidden, obstacles, targetReward, forbiddenReward, movementReward)
     print 'Target: ', target
     print 'Forbidden: ', forbidden
     print 'Start: ', start
-    print 'Obstacles: ', obstacles
-    print 'Obstacle array: ', obstacle
+    print 'Obstacles: ', numberOfObstacles
+    print 'Obstacle array: ', obstacles
     print 'Target reward: ', targetReward
     print 'Forbidden reward: ', forbiddenReward
     print 'Movement reward: ', movementReward
+    print 'Grid: '
+    for line in grid:
+        print line
+    
+def generateGrid(target, forbidden, obstacles, targetReward, forbiddenReward, movementReward):
+    "Grid's rows = y, cols = x and the grid is stored reversed"
+    grid = [[movementReward]*5, [movementReward]*5, [movementReward]*5, [movementReward]*5, [movementReward]*5]
+    grid[target[1]][target[0]] = targetReward
+    grid[forbidden[1]][forbidden[0]] = forbiddenReward
+    for obstacle in obstacles:
+        grid[obstacle[1]][obstacle[0]] = None
+    grid.reverse()
+    return grid
 
 if __name__ == '__main__':
     sys.exit(main())
