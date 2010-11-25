3913f37b4d75a9ac970b966da368608e63e9ffb4
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2010-11-25 16:13:42 -0200

    Recebendo varios targets e forbidden points ao inves de 1

--- src/main.py
+++ src/main.py
@@ -8,9 +8,15 @@ import sys
 def main():
     inputFileName = sys.argv[1]
     inputFile = open(inputFileName, 'r')
-    "Read target coordinates"
-    target = (int(inputFile.readline()),int(inputFile.readline()))
-    forbidden = (int(inputFile.readline()),int(inputFile.readline()))
+    "Read targets coordinates"
+    numberOfTargets = int(inputFile.readline())
+    targets = []
+    for i in range(numberOfTargets):
+        targets.append((int(inputFile.readline()),int(inputFile.readline())))
+    numberOfForbiddens = int(inputFile.readline())
+    forbidden = []
+    for i in range(numberOfForbiddens):
+        forbidden.append((int(inputFile.readline()),int(inputFile.readline())))
     start = (int(inputFile.readline()),int(inputFile.readline()))
     numberOfObstacles = int(inputFile.readline())
     obstacles = []
@@ -19,8 +25,10 @@ def main():
     targetReward = float(inputFile.readline())
     forbiddenReward = float(inputFile.readline())
     movementReward = float(inputFile.readline())
-    grid = generateGrid(target, forbidden, obstacles, targetReward, forbiddenReward, movementReward)
-    print 'Target: ', target
+    grid = generateGrid(targets, forbidden, obstacles, targetReward, forbiddenReward, movementReward)
+    print 'Targets: ', numberOfTargets
+    print 'Target array: ', targets
+    print 'Forbidden points: ', numberOfForbiddens
     print 'Forbidden: ', forbidden
     print 'Start: ', start
     print 'Obstacles: ', numberOfObstacles
@@ -32,11 +40,13 @@ def main():
     for line in grid:
         print line
     
-def generateGrid(target, forbidden, obstacles, targetReward, forbiddenReward, movementReward):
+def generateGrid(targets, forbiddenPoints, obstacles, targetReward, forbiddenReward, movementReward):
     "Grid's rows = y, cols = x and the grid is stored reversed"
     grid = [[movementReward]*5, [movementReward]*5, [movementReward]*5, [movementReward]*5, [movementReward]*5]
-    grid[target[1]][target[0]] = targetReward
-    grid[forbidden[1]][forbidden[0]] = forbiddenReward
+    for target in targets:
+        grid[target[1]][target[0]] = targetReward
+    for forbidden in forbiddenPoints:
+        grid[forbidden[1]][forbidden[0]] = forbiddenReward
     for obstacle in obstacles:
         grid[obstacle[1]][obstacle[0]] = None
     grid.reverse()
