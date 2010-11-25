74506ac9369b97fad1839502a3b1930398aaaf41
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2010-11-25 10:54:26 -0200

    Lendo e parseando o arquivo de entrada

--- src/main.py
+++ src/main.py
@@ -8,8 +8,25 @@ import sys
 def main():
     inputFileName = sys.argv[1]
     inputFile = open(inputFileName, 'r')
-    for line in inputFile:
-        print line,
+    "Read target coordinates"
+    target = (int(inputFile.readline()),int(inputFile.readline()))
+    forbidden = (int(inputFile.readline()),int(inputFile.readline()))
+    start = (int(inputFile.readline()),int(inputFile.readline()))
+    obstacles = int(inputFile.readline())
+    obstacle = []
+    for i in range(obstacles):
+        obstacle.append((int(inputFile.readline()),int(inputFile.readline())))
+    targetReward = float(inputFile.readline())
+    forbiddenReward = float(inputFile.readline())
+    movementReward = float(inputFile.readline())
+    print 'Target: ', target
+    print 'Forbidden: ', forbidden
+    print 'Start: ', start
+    print 'Obstacles: ', obstacles
+    print 'Obstacle array: ', obstacle
+    print 'Target reward: ', targetReward
+    print 'Forbidden reward: ', forbiddenReward
+    print 'Movement reward: ', movementReward
 
 if __name__ == '__main__':
     sys.exit(main())
